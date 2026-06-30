4d85852ec4df984761274adcbac5d1220f0476dd
Saeed Khabbaz Sirjani <saeedkhs1376@gmail.com>
2026-06-30 18:09:45 +0330

    Update core.v

--- src/core.v
+++ src/core.v
@@ -9,7 +9,7 @@ module core(input clk);
   PC mips0(clk,PCin,PCout);
   inst_memory mips1(PCout,instruction);
   reg_file mips2(clk,RegWrite,instruction[25:21],instruction[20:16],write_register,write_data,read_data1,read_data2);
-  ALU mips3(read_data1,ALU_input2,ALU_ctr,ALU_result,zero);
+  ALU mips3(read_data1,ALU_input2,ALU_ctr,ALU_result, ,zero , );
   extension mips4(instruction[15:0],extended);
   control_unit mips5(instruction[31:26],Branch,MemRead,MemWrite,ALUSrc,RegWrite,Jump,MemtoReg,RegDst,AluOp);
   alu_control_unit mips6(AluOp,instruction[5:0],ALU_ctr);
@@ -21,10 +21,11 @@ module core(input clk);
   
   assign write_data=MemtoReg?DataRead:ALU_result;
   assign write_register=RegDst?instruction[15:11]:instruction[20:16];
-  assign PC1=PCout+1;
+  assign PC1=PCout+4;
   assign PCin=Jump?JumpAddress:(Branch&&zero)?BranchTarget:PC1;
   assign ALU_input2=ALUSrc?extended:read_data2;
   assign BranchTarget=extended+PC1;
-  assign JumpAddress={PC1[31:28],2'b00,instruction[25:0]};
+  assign JumpAddress={PC1[31:28],instruction[25:0],2'b00};
 endmodule
+
 
